a188da4b89b13e56424ccd8b9119bc3c34d0d1ac
Softfun <xyf18362929560@163.com>
2017-12-18 21:31:36 +0800

    add library

---
+++ library/src/main/java/com/github/xuyafan/library/LibraryUtils.java
@@ -0,0 +1,12 @@
+package com.github.xuyafan.library;
+
+/**
+ * utils for test
+ */
+
+public class LibraryUtils {
+
+    public static String test(){
+        return "Utils Library Test String 2";
+    }
+}
